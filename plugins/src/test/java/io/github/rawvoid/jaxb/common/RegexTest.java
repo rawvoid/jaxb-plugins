076c8ed0c48c0546feec7a32d01b7fde2d42db40
Rawvoid <rawvoid@users.noreply.github.com>
2025-12-12 16:28:50 +0800

    Add `RegexTest` to verify pattern replacement logic with tests

---
+++ plugins/src/test/java/io/github/rawvoid/jaxb/common/RegexTest.java
@@ -0,0 +1,30 @@
+package io.github.rawvoid.jaxb.common;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Rawvoid
+ */
+public class RegexTest {
+
+    @Test
+    void testReplacePattern() {
+        var pattern = Pattern.compile("(?i)(.+)Type$");
+        String name = "PersonType";
+        String name2 = "PersonType2";
+        String name3 = "PersonTYPE";
+        name = name.replaceAll(pattern.pattern(), "$1");
+        assertThat(name).isEqualTo("Person");
+
+        name2 = name2.replaceAll(pattern.pattern(), "$1");
+        assertThat(name2).isEqualTo("PersonType2");
+
+        name3 = name3.replaceAll(pattern.pattern(), "$1");
+        assertThat(name3).isEqualTo("Person");
+    }
+
+}
